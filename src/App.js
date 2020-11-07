f73a27d6d75941981a075d19bf56bf278b8e35c8
Petr Hovorka <petr.hovorka@ackee.cz>
2020-11-07 01:36:15 +0100

    Send GraphQL query when search field input changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-
 import "./App.css";
 import { Routes } from "./Routes";
 
-const OBEC_QUERY = gql`
-  query Obce_nazvy {
-    obce(obec_nazev: "Horoměřice", datum: "2020-11-05") {
-      obec_nazev
-      aktualne_nemocnych
-    }
-  }
-`;
-
 function App() {
-  const obce = useQuery(OBEC_QUERY, {});
-  if (!obce.loading && !obce.error) {
-    console.log(obce.data.obce);
-  }
   return <Routes />;
 }
 
