bab944b824b7bfae852d1e4cb68c6d2cc1c0e3a6
Petr Hovorka <pehovorka@gmail.com>
2020-12-06 00:16:03 +0100

    Replace also URL in metadata

--- public/index.php
+++ public/index.php
@@ -12,13 +12,13 @@ function getMunicipalityNameFromDB($municipalityCode)
 
     if (is_numeric($municipalityCode)){
     try {
-        $pdo = new PDO('sqlite:.obec_kod_nazev.db');
+        $pdo = new PDO('sqlite:obec_kod_nazev.db');
         $stmt = $pdo->prepare('SELECT obec_nazev FROM obec_kod_nazev WHERE obec_kod = :obec_kod');
         $stmt->execute(array('obec_kod' => $municipalityCode));
 
         $result = $stmt->fetch();
 
-        if ($result[0] !== null) {
+        if ($result !== false ) {
             return $result[0];
         } else {
             return false;
@@ -52,10 +52,19 @@ function getTitle() {
     return "COVID v obcích";
   }
 
+function getUrl() {
+    if (isSharePage()){
+        $uri = $_SERVER["REQUEST_URI"];
+        return "https://covidvobcich.cz" . $uri;
+    }
+    return "https://covidvobcich.cz/";
+}
+
 ob_start(); // Start output buffering
 include "index.html";
 $replacementString = ob_get_clean(); // Store output in variable, and stop output-buffering
-echo str_replace("___TITLE___", getTitle(), $replacementString);
-
+$replacementString = str_replace("___TITLE___", getTitle(), $replacementString);
+$replacementString = str_replace("___URL___", getUrl(), $replacementString);
+echo $replacementString;
 
 ?>
