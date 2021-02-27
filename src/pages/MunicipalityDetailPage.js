a5e952293ad1c6dfc775c3fbfc72531e1c5cd4c2
Petr Hovorka <pehovorka@gmail.com>
2021-02-27 17:16:00 +0100

    Fix days limit select on municipality detail page

--- src/pages/MunicipalityDetailPage.js
+++ src/pages/MunicipalityDetailPage.js
@@ -14,9 +14,13 @@ import { PageNotFound } from "./PageNotFound";
 
 import { LoadingIndicator } from "../components/LoadingIndicator";
 import { TownCard } from "../components/TownCard";
+import { useMunicipalitiesDispatch } from "../providers/MunicipalitiesProvider";
+import { SET_MESSAGE } from "../utils/municipalitiesReducer";
 
 export function MunicipalityDetailPage() {
+  const dispatch = useMunicipalitiesDispatch();
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(90);
   const urlParams = useParams();
 
   const [getMunicipalityName, municipalityName] = useLazyQuery(
@@ -47,8 +51,18 @@ export function MunicipalityDetailPage() {
       setError("Obec s tímto kódem neexistuje!");
     } else if (municipalityName.data) {
       document.title = `${municipalityName.data.obec[0].obec_nazev} – COVID v obcích`;
+    } else if (municipalityName.error) {
+      dispatch({
+        type: SET_MESSAGE,
+        text: "Nepodařilo se připojit k serveru. Zkuste to prosím později.",
+        severity: "error",
+      });
     }
-  }, [municipalityName]);
+  }, [municipalityName, dispatch]);
+
+  const handleDateLimitChange = ({ select }) => {
+    setLimit(select.target.value);
+  };
 
   if (error) {
     return <PageNotFound message={error} />;
@@ -68,6 +82,8 @@ export function MunicipalityDetailPage() {
                   name={municipalityName.data?.obec[0]?.obec_nazev}
                   code={urlParams.code}
                   closeButtonHidden={true}
+                  handleDateLimitChange={handleDateLimitChange}
+                  limit={limit}
                 />
               </Box>
               <Box mt={10}>
