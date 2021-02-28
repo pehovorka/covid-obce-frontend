be10e4c3c387a9cd6987b20d4179b1412fa040f4
Petr Hovorka <pehovorka@gmail.com>
2021-02-28 16:40:35 +0100

    Display last modified date in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Grid, Link } from "@material-ui/core";
+import { Grid, Link, Box } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
 import { CookieDialog } from "./CookieDialog";
 import { route } from "../Routes";
+import { LastModified } from "./LastModified";
 
 export function Footer() {
   const [openCookies, setCookiesOpen] = useState(false);
@@ -16,45 +17,50 @@ export function Footer() {
   };
 
   return (
-    <Grid container justify="center" spacing={5}>
-      <Grid item>
-        <Link component={RouterLink} to={route.info()}>
-          O webu
-        </Link>
+    <>
+      <Grid container justify="center" spacing={5}>
+        <Grid item>
+          <Link component={RouterLink} to={route.info()}>
+            O webu
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => handleClickCookiesOpen()}
+            style={{ verticalAlign: "top" }}
+          >
+            Cookies
+          </Link>
+          <CookieDialog
+            open={openCookies}
+            setOpen={setCookiesOpen}
+            handleClose={handleCookiesClose}
+          />
+        </Grid>
+        <Grid item>
+          Zdroje dat:{" "}
+          <Link
+            href="https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ÚZIS
+          </Link>
+          ,{" "}
+          <Link
+            href="https://www.czso.cz/csu/czso/pocet-obyvatel-v-obcich-k-112019"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ČSÚ
+          </Link>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Link
-          component="button"
-          variant="body2"
-          onClick={() => handleClickCookiesOpen()}
-          style={{ verticalAlign: "top" }}
-        >
-          Cookies
-        </Link>
-        <CookieDialog
-          open={openCookies}
-          setOpen={setCookiesOpen}
-          handleClose={handleCookiesClose}
-        />
-      </Grid>
-      <Grid item>
-        Zdroje dat:{" "}
-        <Link
-          href="https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19"
-          target="_blank"
-          rel="noreferrer"
-        >
-          ÚZIS
-        </Link>
-        ,{" "}
-        <Link
-          href="https://www.czso.cz/csu/czso/pocet-obyvatel-v-obcich-k-112019"
-          target="_blank"
-          rel="noreferrer"
-        >
-          ČSÚ
-        </Link>
-      </Grid>
-    </Grid>
+      <Box pt={2}>
+        <LastModified />
+      </Box>
+    </>
   );
 }
