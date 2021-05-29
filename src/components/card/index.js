4704c7ed0773f2d922269e54e2579745ed61ad90
Petr Hovorka <pehovorka@gmail.com>
2021-05-29 17:11:43 +0200

    :bug: Fix lazy loading of components

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,7 +1,5 @@
-export { default as ActivePer1000 } from "./ActivePer1000";
 export { default as DateLimitSelect } from "./DateLimitSelect";
 export { default as DragAndDropCards } from "./DragAndDropCards";
-export { default as Chart } from "./Chart";
 export { default as ChartTooltip } from "./ChartTooltip";
 export { default as MunicipalityCard } from "./MunicipalityCard";
 export { default as MunicipalityStats } from "./MunicipalityStats";
