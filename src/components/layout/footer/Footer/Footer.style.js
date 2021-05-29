943209f5f0bc1d80e062305d56890a5afc46ef3c
Petr Hovorka <pehovorka@gmail.com>
2021-05-29 16:54:45 +0200

    :art: Don't ignore case sensitivity one more time

---
+++ src/components/layout/footer/Footer/Footer.style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles(() => ({
+  container: {
+    borderTop: "5px solid #efefef",
+  },
+  content: {
+    padding: "2rem",
+    margin: "0 auto",
+    width: "70%",
+    fontSize: "1rem",
+  },
+  link: {
+    fontWeight: 500,
+  },
+}));
---
+++ src/components/layout/footer/index.js
@@ -0,0 +1,3 @@
+export { default as CookieDialog } from "./CookieDialog";
+export { default as Footer } from "./Footer";
+export { default as LastModified } from "./LastModified";
