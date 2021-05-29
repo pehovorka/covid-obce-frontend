6a0265128903908e6651117a5b24acffaf0d5310
Petr Hovorka <pehovorka@gmail.com>
2021-05-29 16:19:43 +0200

    :bug: Fix git folders duplication 1/2

--- src/components/layout/appBar/index.js
+++
@@ -1,3 +0,0 @@
-export { default as AppBar } from "./AppBar";
-export { default as SearchButton } from "./SearchButton";
-export { default as SearchField } from "./SearchField";
--- src/components/layout/footer/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CookieDialog } from "./CookieDialog";
-export { default as Footer } from "./Footer";
-export { default as LastModified } from "./LastModified";
