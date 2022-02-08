2e29e57787562f534cc804ded61718d715a91dd5
Petr Hovorka <pehovorka@gmail.com>
2022-02-08 00:09:22 +0000

    ✨ Add map loading progress bar

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { Box, Grid, LinearProgress, Typography } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import "leaflet/dist/leaflet.css";
 
 import municipalitiesTopo from "../../assets/municipalitiesTopo.json";
 import TopoJSON from "./TopoJSON";
 import { useQuery } from "@apollo/client";
 import { MUNICIPALITY_OVERVIEW_QUERY } from "../../utils/queries";
-import { Alert } from "@material-ui/lab";
+import { useStyles } from "./Map.style";
 
 function Map() {
+  const classes = useStyles();
+
   const bounds = [
     [51.06, 18.86],
     [48.55, 12.09],
@@ -18,35 +22,54 @@ function Map() {
     fetchPolicy: "cache-first",
   });
 
-  if (municipalityCasesOverview.loading && !municipalityCasesOverview.data) {
-    return null;
-  }
   if (municipalityCasesOverview.error) {
     return (
-      <Alert severity="error">
+      <Alert severity="error" className={classes.error}>
         Chyba při načítání dat. Zkuste to prosím znovu později.
       </Alert>
     );
   }
 
   return (
-    <MapContainer
-      style={{ height: "calc(100vh - 72px)" }}
-      scrollWheelZoom={false}
-      bounds={bounds}
-      boundsOptions={{ maxZoom: 10 }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <TopoJSON
-        data={municipalitiesTopo}
-        municipalityCasesOverview={
-          municipalityCasesOverview.data.municipalityCasesOverview
-        }
-      />
-    </MapContainer>
+    <Box className={classes.container}>
+      {municipalityCasesOverview.loading && !municipalityCasesOverview.data ? (
+        <Grid
+          container
+          justifyContent="center"
+          alignContent="center"
+          spacing={6}
+          style={{ height: "100%" }}
+        >
+          <Grid item xs={8}>
+            <Typography component="p" variant="h5" align="center">
+              Mapa se načítá...
+            </Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <LinearProgress />
+          </Grid>
+        </Grid>
+      ) : (
+        <MapContainer
+          style={{ height: "100%" }}
+          scrollWheelZoom={false}
+          bounds={bounds}
+          boundsOptions={{ maxZoom: 10 }}
+          zoomSnap={0.25}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <TopoJSON
+            data={municipalitiesTopo}
+            municipalityCasesOverview={
+              municipalityCasesOverview.data.municipalityCasesOverview
+            }
+          />
+        </MapContainer>
+      )}
+    </Box>
   );
 }
 
---
+++ src/components/map/Map.style.js
@@ -0,0 +1,8 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "calc(100vh - 72px)",
+  },
+  error: { margin: 20 },
+}));
